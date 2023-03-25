2982f0dae0bf0a5640576e20923fd96c96ee8c94
GiancarloF20 <giancarlofarfan20@gmail.com>
2023-03-24 23:22:11 -0500

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿function calcularPago() {
+    const montoPagar = document.getElementById('montoPagar').value;
+    const diasRetraso = document.getElementById('dias_retraso').value;
+    const intereses = 0.005; // Tasa de interés diaria (0.5%)
+
+    const mora = montoPagar * (intereses * diasRetraso);
+    const totalPagar =(montoPagar+(mora.toFixed(2))*10)/10;
+    
+
+    document.getElementById('mora').value = mora.toFixed(2);
+    document.getElementById('totalPagar').value = totalPagar.toFixed(2);
+}
